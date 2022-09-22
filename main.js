29dfda274c3825cc13389199f1d4a2f45725bd27
davronnnn <kifleader@gmail.com>
2022-09-22 13:38:20 +0500

    last

---
+++ main.js
@@ -0,0 +1,23 @@
+// alert('Hello world!');
+
+// var number1 = prompt(`1- qiymatni kiriting`);
+// var number2 = prompt('2- qiymatni kiriting');
+
+// var result = Number(number1) + Number(number2);
+// console.log(result);
+
+// var a = 10;
+// var b = 5;
+
+// console.log(a + b);
+var result1 = 0.3 - 0.2;
+var result2 = 0.2 - 0.1;
+
+let a = 3 * 2 ** 3 * 3;
+
+console.log(a);
+
+console.log(result1, result2);
+
+// var a = 8 * 4 / 2 / 2;
+// alert(a);
