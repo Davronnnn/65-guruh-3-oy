9a735c5f2be7b11d4e83dd9b04a2e8c2a00ba056
davronnnn <kifleader@gmail.com>
2022-09-24 13:02:49 +0500

    main

--- main.js
+++ main.js
@@ -10,14 +10,82 @@
 // var b = 5;
 
 // console.log(a + b);
-var result1 = 0.3 - 0.2;
-var result2 = 0.2 - 0.1;
 
-let a = 3 * 2 ** 3 * 3;
+// Nan not a number
 
-console.log(a);
+// console.log(name2 + 2);
 
-console.log(result1, result2);
+//  Davron Sulaymonov
 
+// console.log('Davron' + 'Sulaymonov');
+//          12 - 2 = 10
+//  string + number = string
+// number + number = number;
+// string + string = string;
+
+// console.log('12' - '2');
+
+// var age = Number(prompt('yoshingizni kiriting'));
+// var year = Number(prompt('yil kiriting'));
+
+// var result = year - 2022 + age;
+// var result2 = age++;
+// var a = 2;
+// console.log(a++); // 2
+// console.log(a++); // 3
+// console.log(a);
+
+// var number = 12;
+// // number++;
+
+// number -= 12; // number = number + 12
+
+// // alert(`sizning ${year}-yildagi yoshingiz ${result} `);
+// console.log(number);
+// console.log(number);
+// alert('1 yildan keyingi yoshingiz' +  result2);
 // var a = 8 * 4 / 2 / 2;
 // alert(a);
+
+// var number = -12.5999993;
+// console.log(number);
+// var result = Math.floor(number);
+// var result2 = Math.ceil(number);
+// var result3 = Math.round(number);
+
+// console.log(result, result2, result3);
+
+// var randomNumber = Math.floor(Math.random() * 10) + 1; // [0,10] ,
+
+// var number = (12.23123123123).toFixed(5);
+// console.log(number);
+
+// var userMoney = prompt('Pulingizni kiriting');
+
+// const usd = 11650.34;
+// const euro = 10650.03;
+
+// var ticket = 500;
+// var hotel = 250;
+
+// var fun = 120;
+
+// var usdToSum = (ticket + hotel) * usd;
+// var euroToSum = fun * euro;
+
+// var totalAmount = usdToSum + euroToSum;
+// console.log(userMoney);
+// alert(userMoney - totalAmount + ' mana shuncha pulingiz ortib qoldi');
+
+// var user = 'Number1';
+// var user = 'Number2';
+// user = 'boshqa qiymat';
+
+let name = 'Davron';
+//let name = 'Temur'; // redeclare qilib bo'lmaydi
+name = 'Temur';
+
+const surname = 'Temuraliyev';
+// const surname = 'boshqa';
+// surname = 'Norqulov';
+console.log(surname);
